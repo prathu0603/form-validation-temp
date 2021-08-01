5285e52a97aa90f73d3b722dfaa3b7cf16bb8c05
prathu0603 <66918038+prathu0603@users.noreply.github.com>
2021-08-01 12:56:17 +0530

    Update Signup.js

--- src/Components/Signup.js
+++ src/Components/Signup.js
@@ -4,13 +4,13 @@ import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const Signup = () => {
-  const validate = Yup.object().shape({
+  const validate = Yup.object({
     name: Yup.string()
       .max(15, "Must Be 15 Characters Or Less")
       .required("Required"),
     email: Yup.string().email("Enter A Valid Email").required("Required"),
     password: Yup.string()
-      .max(6, "Must Be 6 Characters Or More")
+      .min(6, "Password Must Be Atleast 6 Character Long")
       .required("Required"),
     cpassword: Yup.string()
       .oneOf([Yup.ref("password"), null], "Password Must Match")
@@ -25,15 +25,20 @@ const Signup = () => {
     },
     onSubmit: (values) => {
       alert(JSON.stringify(values, null, 2));
+      console.log(values.name);
     },
     validationSchema: validate,
   });
 
   return (
-    <div className="container mt-3">
+    <div className="container my-5">
       <div className="row">
         <div className="col-md-5">
-          <form method="POST" onSubmit={formik.handleSubmit}>
+          <form
+            method="POST"
+            onSubmit={formik.handleSubmit}
+            onReset={formik.resetForm}
+          >
             <label>First Name</label>
             <input
               type="text"
@@ -41,34 +46,74 @@ const Signup = () => {
               value={formik.values.name}
               onChange={formik.handleChange}
               placeholder="Rahul"
-              className="form-control shadow-none"
+              className={`form-control shadow-none ${
+                formik.touched.name && formik.errors.name && "is-invalid"
+              }`}
+              onBlur={formik.handleBlur}
             />
+            <div className="text-danger  mb-2 ">
+              {formik.touched.name && formik.errors.name
+                ? formik.errors.name
+                : ""}
+            </div>
+
             <label>Email</label>
             <input
               type="email"
               name="email"
               value={formik.values.email}
               onChange={formik.handleChange}
-              className="form-control shadow-none"
+              className={`form-control shadow-none ${
+                formik.touched.email && formik.errors.email && "is-invalid"
+              }`}
+              onBlur={formik.handleBlur}
             />
+            <div className="text-danger mb-2">
+              {formik.touched.email && formik.errors.email
+                ? formik.errors.email
+                : ""}
+            </div>
             <label>Password</label>
             <input
               type="password"
               name="password"
               value={formik.values.password}
               onChange={formik.handleChange}
-              className="form-control shadow-none"
+              className={`form-control shadow-none ${
+                formik.touched.password &&
+                formik.errors.password &&
+                "is-invalid"
+              }`}
+              onBlur={formik.handleBlur}
             />
+            <div className="text-danger mb-2">
+              {formik.touched.password && formik.errors.password
+                ? formik.errors.password
+                : ""}
+            </div>
             <label>Confirm Password</label>
             <input
               type="password"
               name="cpassword"
               value={formik.values.cpassword}
               onChange={formik.handleChange}
-              className="form-control shadow-none"
+              className={`form-control shadow-none ${
+                formik.touched.cpassword &&
+                formik.errors.cpassword &&
+                "is-invalid"
+              }`}
+              onBlur={formik.handleBlur}
             />
+            <div className="text-danger mb-2">
+              {formik.touched.cpassword && formik.errors.cpassword
+                ? formik.errors.cpassword
+                : ""}
+            </div>
             <button type="submit" className="btn btn-dark mt-3">
               Register
+            </button>
+            <button className="btn btn-danger mt-3 mx-5" type="reset">
+              Reset
             </button>
           </form>
         </div>
